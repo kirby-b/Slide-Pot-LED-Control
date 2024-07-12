5e8c4a7ad4fa2822cd5898116d815d27a0e46ba3
kirby-b <kirby.brodie66@gmail.com>
2024-07-12 15:09:33 -0400

    Now uses array to make it more clean

--- sketch_Slide_Pot_LED/sketch_Slide_Pot_LED.ino
+++ sketch_Slide_Pot_LED/sketch_Slide_Pot_LED.ino
@@ -1,11 +1,15 @@
 #define PIN_SLIDE_POT_A A0
 
-int LED_RED_1 = 2;
-int LED_RED_2 = 3;
-int LED_YELLOW_1 = 4;
-int LED_YELLOW_2 = 5;
-int LED_GREEN_1 = 6;
-int LED_GREEN_2 = 7;
+int LED_RED_1 = 3;
+int LED_RED_2 = 2;
+int LED_YELLOW_1 = 5;
+int LED_YELLOW_2 = 4;
+int LED_GREEN_1 = 7;
+int LED_GREEN_2 = 6;
+int arr_len = 6;
+
+int leds[] = {LED_GREEN_1,LED_GREEN_2,LED_YELLOW_1,LED_YELLOW_2,LED_RED_1,LED_RED_2};
+String values[] = {"170","340","510","680","850","1020"};
 
 void setup() {
   Serial.begin(9600);
@@ -22,41 +26,12 @@ void loop() {
   int slide_value = analogRead(PIN_SLIDE_POT_A);
   Serial.print("Slide Pot value: ");
   Serial.println(slide_value);
-  if (slide_value > 170){
-    digitalWrite(LED_GREEN_2, HIGH);
-  }
-  else{
-    digitalWrite(LED_GREEN_2, LOW);
-  }  
-  if (slide_value > 340){
-    digitalWrite(LED_GREEN_1, HIGH);
-  }
-  
-  else{
-    digitalWrite(LED_GREEN_1, LOW);
-  } 
-  if (slide_value > 510){
-    digitalWrite(LED_YELLOW_2, HIGH);
-  }
-  else{
-    digitalWrite(LED_YELLOW_2, LOW);
-  } 
-  if (slide_value > 680){
-    digitalWrite(LED_YELLOW_1, HIGH);
-  }
-  else{
-    digitalWrite(LED_YELLOW_1, LOW);
-  }
-  if (slide_value > 850){
-    digitalWrite(LED_RED_2, HIGH);
-  }
-  else{
-    digitalWrite(LED_RED_2, LOW);
-  }
-  if (slide_value > 1020){
-    digitalWrite(LED_RED_1, HIGH);
-  }
-  else{
-    digitalWrite(LED_RED_1, LOW);
+  for ( int i = 0; i < arr_len; ++i ){
+    if (slide_value > values[i].toInt()){
+      digitalWrite(leds[i], HIGH);
+    }
+    else{
+      digitalWrite(leds[i], LOW);
+    } 
   }
 }
